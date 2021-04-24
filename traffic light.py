d2f54eb06cbeeacf74ce8355c6352c2c06d696d9
ferhadquluzade <62217940+ferhadquluzade@users.noreply.github.com>
2021-04-24 15:46:01 +0400

    Update traffic light.py

--- traffic light.py
+++ traffic light.py
@@ -1,19 +1,18 @@
-import RPi.GPIO as GPIO
+from gpiozero import LED
 from time import sleep 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4,0)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(27,0)
+green=LED(4)
+yellow=LED(17)
+red=LED(27)
 while 1:
-	GPIO.output(4,1)
-	sleep(2)
-	GPIO.output(17,1)
-	sleep(2)                                                                                                                                                                                                                                                                         
-	GPIO.output(27,1)
-	sleep(2)                                                                                                                                                                                                                                                     
-	GPIO.output(4,0)                                                                                                                                                                                                                                                                
-	GPIO.output(17,0)                                                                                                                                                                                                                                                                
-	GPIO.output(27,0)     
-	sleep(2)                                                                                                                                                                                                                                                     
-	                                                                                                                                                                                                                                                           
-	
+	green.on()
+	sleep(2)                                                                                                                                                                                                                                    
+	green.off()
+	yellow.on()
+	sleep(2)     	
+	yellow.off()
+	red.on()
+	sleep(2)                       
+	red.off()
+	sleep(2)                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+
+
